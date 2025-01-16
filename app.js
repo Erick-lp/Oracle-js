43962b2c5e1ff48c6d6c7d25854a6fd43ba788f2
Erick Lopez <yupdenilson@gmail.com>
2025-01-16 16:47:51 -0600

    Funcion agregar amigo

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 let amigos = [];
+let longitudArray;
+
 
 function agregarAmigo(){
     amigoNuevo = document.getElementById("amigo").value;
-    amigos.push(amigoNuevo);
+    if(amigoNuevo.trim() !== ""){
+        amigos.push(amigoNuevo);
+        longitudArray = amigos.length;
+        console.log(longitudArray);
+        asignarTextoElemento("#listaAmigos", amigos.join(", "));
+    } else {
+        alert("Debes ingresar un nombre");
+    }
+}
+
+
+
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
 }
