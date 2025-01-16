a236bbace0e4e54f4df73be1f383f1e9bdebf436
Erick Lopez <yupdenilson@gmail.com>
2025-01-16 17:11:25 -0600

    Creacion de la funcion sortear amigos

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 let amigos = [];
 let longitudArray;
-
+let amigoSeleccionado;
+let nombreseleccionado;
 
 function agregarAmigo(){
+    asignarTextoElemento("#resultado", " ");
     amigoNuevo = document.getElementById("amigo").value;
     if(amigoNuevo.trim() !== ""){
         amigos.push(amigoNuevo);
         longitudArray = amigos.length;
-        console.log(longitudArray);
+        document.querySelector('#amigo').value = '';
         asignarTextoElemento("#listaAmigos", amigos.join(", "));
+        document.getElementById('sortear').removeAttribute('disabled');
     } else {
         alert("Debes ingresar un nombre");
     }
 }
 
-
+function sortearAmigo(){
+    amigoSeleccionado = Math.floor(Math.random()*longitudArray);
+    nombreseleccionado = amigos[amigoSeleccionado];
+    asignarTextoElemento("#listaAmigos", " ");
+    asignarTextoElemento("#resultado", `El amigo secreto sorteado es: ${nombreseleccionado}`);
+    amigos = [];
+    document.querySelector('#sortear').setAttribute('disabled','true');
+}
 
 
 function asignarTextoElemento(elemento, texto) {
